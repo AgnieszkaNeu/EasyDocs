8dd4e56615d22d3ec1992068305f84748da0759f
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-02-24 17:08:34 +0100

    Change collection type

--- src/main/java/com/example/easyDocs/Document/DocumentRepository.java
+++ src/main/java/com/example/easyDocs/Document/DocumentRepository.java
@@ -3,11 +3,11 @@ package com.example.easyDocs.Document;
 import com.example.easyDocs.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface DocumentRepository extends JpaRepository<Document,Long> {
     Optional<Document> findByName(String name);
-    List<Document> findAllByName(String name);
-    List<Document> findAllByCreator(User creator);
+    Set<Document> findAllByName(String name);
+    Set<Document> findAllByCreator(User creator);
 }
